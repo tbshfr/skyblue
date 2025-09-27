602e9eaecde47f47c7c6094d01e97d951fb24f10
tbshfr <noreply@git.tbshfr.com>
2025-09-27 19:53:59 +0200

    chore: mask countme timer

--- build/scripts/08-enable-services.sh
+++ build/scripts/08-enable-services.sh
@@ -4,3 +4,5 @@ set -eux -o pipefail
 systemctl enable dconf-update.service
 systemctl enable apply-kargs.service
 systemctl enable rpm-ostreed-automatic.timer
+
+systemctl mask rpm-ostree-countme.timer
