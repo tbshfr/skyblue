f6e288b689f909a6803d7d366bf6fdd747172e82
tbshfr <noreply@git.tbshfr.com>
2025-10-05 14:43:09 +0200

    chore: add build date

--- build/scripts/09-other.sh
+++ build/scripts/09-other.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -eux -o pipefail
 
-sed -i "s,^PRETTY_NAME=.*,PRETTY_NAME=\"skyblue (Fedora Linux $(rpm -E %fedora))\"," /usr/lib/os-release
+sed -i "s,^PRETTY_NAME=.*,PRETTY_NAME=\"skyblue-$(rpm -E %fedora)-$(date +%Y%m%d)\"," /usr/lib/os-release
